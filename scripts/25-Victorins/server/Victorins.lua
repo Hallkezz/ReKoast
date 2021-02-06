69df948ee22ba2d69dec1456605d3f65a14f7c0f
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2021-02-06 12:15:07 +0300

    Update Victorins.lua
    Добавлено больше различных математических примеров.

--- scripts/25-Victorins/server/Victorins.lua
+++ scripts/25-Victorins/server/Victorins.lua
@@ -12,19 +12,7 @@ end
 
 function Victorins:PostTick( args )
 	if self.timer:GetMinutes() <= 15 then return end
-	local first, second = math.random( 100, 999 ), math.random( 100, 999 )
-	self.quizAnswer = first + second
-	self.reward = rewards[math.random(#rewards)]
-
-	for p in Server:GetPlayers() do
-		if p:GetValue( "Lang" ) == "ENG" then
-			p:SendChatMessage( "[Quiz] ", Color.White, "Who first write reply right will get $" .. self.reward .. "!", Color.Yellow )
-			p:SendChatMessage( "[Quiz] ", Color.White, first .. " + " .. second .. " = ???", Color.Yellow )
-		else
-			p:SendChatMessage( "[Викторина] ", Color.White, "Первый, кто напишет ответ - получит $" .. self.reward .. "!", Color.Yellow )
-			p:SendChatMessage( "[Викторина] ", Color.White, first .. " + " .. second .. " = ???", Color.Yellow )
-		end
-	end
+	self:StartMath()
 	self.timer:Restart()
 end
 
@@ -40,6 +28,62 @@ function Victorins:PlayerChat( args )
 			end
 			args.player:SetMoney( args.player:GetMoney() + self.reward )
 			self.quizAnswer = nil
+		end
+	end
+end
+
+function Victorins:StartMath()
+	local Type = math.random(1,6) 
+
+	if Type == 1 then
+		local first, second = math.random( 100, 999 ), math.random( 100, 999 )
+		self.mathtype = tostring( first .. " + " .. second )
+		self.quizAnswer = first + second
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	if Type == 2 then 
+		local first, second = math.random( 100, 999 ), math.random( 100, 999 )
+		self.mathtype = tostring( first .. " - " .. second )
+		self.quizAnswer = first - second
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	if Type == 3 then 
+		local first, second, third = math.random( 1, 99 ), math.random( 1, 99 ), math.random( 1, 99 )
+		self.mathtype = tostring( first .. " - " .. second .. " - " .. third )
+		self.quizAnswer = first - second - third
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	if Type == 4 then 
+		local first, second, third = math.random( 1, 99 ), math.random( 1, 99 ), math.random( 1, 99 )
+		self.mathtype = tostring( first .. " + " .. second .. " + " .. third )
+		self.quizAnswer = first + second + third
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	if Type == 5 then 
+		local first, second, third = math.random( 1, 99 ), math.random( 1, 99 ), math.random( 1, 99 )
+		self.mathtype = tostring( first .. " - " .. second .. " + " .. third )
+		self.quizAnswer = first - second + third
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	if Type == 6 then 
+		local first, second, third = math.random( 1, 99 ), math.random( 1, 99 ), math.random( 1, 99 )
+		self.mathtype = tostring( first .. " + " .. second .. " - " .. third )
+		self.quizAnswer = first - second + third
+		self.reward = rewards[math.random(#rewards)]
+	end
+
+	for p in Server:GetPlayers() do
+		if p:GetValue( "Lang" ) == "ENG" then
+			p:SendChatMessage( "[Quiz] ", Color.White, "Who first write reply right will get $" .. self.reward .. "!", Color.Yellow )
+			p:SendChatMessage( "[Quiz] ", Color.White, self.mathtype .. " = ???", Color.Yellow )
+		else
+			p:SendChatMessage( "[Викторина] ", Color.White, "Первый, кто напишет ответ - получит $" .. self.reward .. "!", Color.Yellow )
+			p:SendChatMessage( "[Викторина] ", Color.White, self.mathtype .. " = ???", Color.Yellow )
 		end
 	end
 end
