a9ae3fc3b4b88233fb62020b9523137561518e78
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2021-02-16 20:35:46 +0300

    Update cars.lua
    - В разы упрощен код.
    - Добавлены сообщения при включении/отключении.

--- scripts/TestingFunctions/RainbowCars/server/cars.lua
+++ scripts/TestingFunctions/RainbowCars/server/cars.lua
@@ -1,55 +1,34 @@
 class "RainbowCars"
 
 function RainbowCars:__init()
-    self.RainbowPlayers = {}
+    self.prefix = "[Радуга] "
 
     self.rT = Timer()
 
     Events:Subscribe( "PreTick", self, self.PreTick )
     Events:Subscribe( "PlayerChat", self, self.PlayerChat )
-    Events:Subscribe( "PlayerQuit", self, self.PlayerQuit )
-    Events:Subscribe( "RAddPlayer", self, self.RAddPlayer )
-    Events:Subscribe( "RDelPlayer", self, self.RDelPlayer )
-end
-
-function RainbowCars:PlayerQuit( args )
-    self:RDelPlayer( args.player:GetId() )
 end
 
 function RainbowCars:PlayerChat( args )
-    if args.text == "/rnb 1" then
-        self:RAddPlayer( args.player:GetId() )
-    end
-
-    if args.text == "/rnb 0" then
-        self:RDelPlayer( args.player:GetId() )
-    end
-end
-
-function RainbowCars:RAddPlayer( id )
-    for i, val in ipairs( self.RainbowPlayers ) do
-        if val == id then
-            return
-        end
-    end
-    table.insert( self.RainbowPlayers, id )
-end
-
-function RainbowCars:RDelPlayer( id )
-    for i, val in ipairs( self.RainbowPlayers ) do
-        if val == id then
-            table.remove( self.RainbowPlayers, i )
+    if args.text == "/rnb" then
+        if args.player:GetValue( "RainbowCar" ) then
+            args.player:SetNetworkValue( "RainbowCar", nil )
+            Chat:Send( args.player, self.prefix, Color.White, "Переливание цвета транспорта отключено.", Color.Pink )
+        else
+            args.player:SetNetworkValue( "RainbowCar", 1 )
+            Chat:Send( args.player, self.prefix, Color.White, "Переливание цвета транспорта включено.", Color.Pink )
         end
     end
 end
 
 function RainbowCars:Colorim()
-    for i, val in ipairs( self.RainbowPlayers ) do
-        local p = Player.GetById(val)
-        if p:InVehicle() then
-            local h = ( 0.01 * self.rT:GetMilliseconds() - string.len( p:GetName() ) ) * 10
-            local color = Color.FromHSV( h % 360, 1, 1 )
-            p:GetVehicle():SetColors( color, color )
+    for p in Server:GetPlayers() do
+        if p:GetValue( "RainbowCar" ) then
+            if p:InVehicle() then
+                local h = ( 0.01 * self.rT:GetMilliseconds() - string.len( p:GetName() ) ) * 10
+                local color = Color.FromHSV( h % 360, 1, 1 )
+                p:GetVehicle():SetColors( color, color )
+            end
         end
     end
 end
