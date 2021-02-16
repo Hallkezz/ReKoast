15b55c9d8553efece4e6cd86c2d0018446c1a0f3
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2021-02-16 20:07:48 +0300

    Update cars.lua
    Оформил код под стиль других скриптов.

--- scripts/TestingFunctions/RainbowCars/server/cars.lua
+++ scripts/TestingFunctions/RainbowCars/server/cars.lua
@@ -1,59 +1,63 @@
-local RainbowPlayers = {}
 class "RainbowCars"
 
 function RainbowCars:__init()
-    Chat:Broadcast("Reload", Color.Green)
+    self.RainbowPlayers = {}
+
     self.timer = Timer()
     self.rT = Timer()
-    Events:Subscribe("PreTick", self, self.PreTick)
-    Events:Subscribe("PlayerChat", self, self.PlayerChat)
-    Events:Subscribe("PlayerQuit", self, self.PlayerQuit)
-    Events:Subscribe("RAddPlayer", self, self.RAddPlayer)
-    Events:Subscribe("RDelPlayer", self, self.RDelPlayer)
-end
-function RainbowCars:PlayerQuit(args)
-    self:RDelPlayer(args.player:GetId())
+
+    Events:Subscribe( "PreTick", self, self.PreTick )
+    Events:Subscribe( "PlayerChat", self, self.PlayerChat )
+    Events:Subscribe( "PlayerQuit", self, self.PlayerQuit )
+    Events:Subscribe( "RAddPlayer", self, self.RAddPlayer )
+    Events:Subscribe( "RDelPlayer", self, self.RDelPlayer )
 end
 
-function RainbowCars:PlayerChat(args)
+function RainbowCars:PlayerQuit( args )
+    self:RDelPlayer( args.player:GetId() )
+end
+
+function RainbowCars:PlayerChat( args )
     if args.text == "/rnb 1" then
-        self:RAddPlayer(args.player:GetId())
+        self:RAddPlayer( args.player:GetId() )
     end
+
     if args.text == "/rnb 0" then
-        self:RDelPlayer(args.player:GetId())
+        self:RDelPlayer( args.player:GetId() )
     end
 end
 
-function RainbowCars:RAddPlayer(id)
-    for i, val in ipairs(RainbowPlayers) do
+function RainbowCars:RAddPlayer( id )
+    for i, val in ipairs( self.RainbowPlayers ) do
         if val == id then
             return
         end
     end
-    table.insert(RainbowPlayers, id)
+    table.insert( self.RainbowPlayers, id )
 end
 
-function RainbowCars:RDelPlayer(id)
-    for i, val in ipairs(RainbowPlayers) do
+function RainbowCars:RDelPlayer( id )
+    for i, val in ipairs( self.RainbowPlayers ) do
         if val == id then
-            table.remove(RainbowPlayers, i)
+            table.remove( self.RainbowPlayers, i )
         end
     end
 end
 
 function RainbowCars:Colorim()
-    for i, val in ipairs(RainbowPlayers) do
+    for i, val in ipairs( self.RainbowPlayers ) do
         local p = Player.GetById(val)
         if p:InVehicle() then
-            local h = (0.01 * self.rT:GetMilliseconds() - string.len(p:GetName())) * 10
-            local color = Color.FromHSV(h % 360, 1, 1)
-            p:GetVehicle():SetColors(color, color)
+            local h = ( 0.01 * self.rT:GetMilliseconds() - string.len( p:GetName() ) ) * 10
+            local color = Color.FromHSV( h % 360, 1, 1 )
+            p:GetVehicle():SetColors( color, color )
         end
     end
 end
 
-function RainbowCars:PreTick(args)
+function RainbowCars:PreTick( args )
     self:Colorim()
     self.timer:Restart()
 end
+
 rainbowcars = RainbowCars()
